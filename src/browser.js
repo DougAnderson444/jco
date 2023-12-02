8e213f30128db15792e1a22050146d913728fa43
Doug Anderson444 <douganderson444@gmail.com>
2023-12-02 17:56:58 -0400

    replace top level await with Promise
    Seems to work better?

--- src/browser.js
+++ src/browser.js
@@ -1,3 +1,18 @@
 import { $init, generate, generateTypes } from '../obj/js-component-bindgen-component.js';
-await $init;
-export { generate as transpile, generateTypes }
+let initialized = new Promise((resolve, reject) => {
+  // Doing this enables us to avoid the top level await that browsers currently don't support
+  $init.then(() => {
+    resolve(true);
+  });
+});
+
+export const transpile = function (component, options) {
+  // if $init has not yet resolved, wait for it to resolve
+  // before calling generate
+  // if it has already resolved, pass args directly to generate and retun the result
+  return initialized.then(() => {
+    return generate(component, options);
+  });
+};
+
+export { generateTypes };
