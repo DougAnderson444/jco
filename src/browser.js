7f28e261811d7d9e58e5c8c78c48d4a08859b4bf
Doug Anderson444 <douganderson444@gmail.com>
2023-12-03 08:11:34 -0400

    initialize as anonymous function returning a Promise

--- src/browser.js
+++ src/browser.js
@@ -6,6 +6,15 @@ let initialized = new Promise((resolve, reject) => {
   });
 });
 
+// The anonymous function will return a promise, which we will await before calling generate
+let initialized = (() =>
+  new Promise((resolve, reject) => {
+    // Doing this enables us to avoid the top level await that browsers currently don't support
+    $init.then(() => {
+      resolve(true);
+    });
+  }))();
+
 export const transpile = function (component, options) {
   // if $init has not yet resolved, wait for it to resolve
   // before calling generate
