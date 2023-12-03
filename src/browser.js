5b0c0f755a3bb8527f424719a62fa3b0c55c0ed4
Doug Anderson444 <douganderson444@gmail.com>
2023-12-03 08:15:34 -0400

    cleanup changes

--- src/browser.js
+++ src/browser.js
@@ -1,10 +1,3 @@
-import { $init, generate, generateTypes } from '../obj/js-component-bindgen-component.js';
-let initialized = new Promise((resolve, reject) => {
-  // Doing this enables us to avoid the top level await that browsers currently don't support
-  $init.then(() => {
-    resolve(true);
-  });
-});
 
 // The anonymous function will return a promise, which we will await before calling generate
 let initialized = (() =>
@@ -18,7 +11,8 @@ let initialized = (() =>
 export const transpile = function (component, options) {
   // if $init has not yet resolved, wait for it to resolve
   // before calling generate
-  // if it has already resolved, pass args directly to generate and retun the result
+  // if it has already resolved, `then` will be called right away
+  // and pass args directly to generate, returning the result
   return initialized.then(() => {
     return generate(component, options);
   });
